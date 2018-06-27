9690b124a741439f406aa9a8e543a415330c0167
amygreen <ayam.greental@gmail.com>
2018-06-27 13:43:49 +0300

    preprocessing - brain segmentation stage

---
+++ PythonHackathon/Preprocessing.py
@@ -0,0 +1,56 @@
+import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
+import os
+from dipy.segment.mask import median_otsu
+from dipy.core.histeq import histeq
+
+
+bvecs_file='/Users/ayam/Documents/PythonHackathon/Data/Stroke/files/bvecs'
+bvals_file='/Users/ayam/Documents/PythonHackathon/Data/Stroke/files/bvals'
+dti4d_file='/Users/ayam/Documents/PythonHackathon/Data/Stroke/files/DTI4D.nii.gz'
+mni_template='/Users/ayam/Documents/PythonHackathon/Data/mni151_2mm.nii'
+
+class Preprocessing():
+
+    def __init__(self,bvecs_file, bvals_file, dti4d_file, mni_template):
+        self.bvecs_file = bvecs_file
+        self.bvals_file = bvals_file
+        self.dti4d_file = dti4d_file
+        self.mni_template = mni_template
+        self.img = nib.load(self.dti4d_file)
+        self.data = self.img.get_data()
+
+
+    def brain_segmentation(self):
+        """
+        This function does brain segmentation using Dipy - median_otsu
+        :return:
+        Two nifti files - binary mask and the brain mask
+        """
+        self.data=self.data[:,:,:,4]
+        self.b0_mask, self.mask = median_otsu(self.data, 2, 1)
+        self.mask_img = nib.Nifti1Image(self.mask.astype(np.float32), self.img.affine)
+        self.b0_img = nib.Nifti1Image(self.b0_mask.astype(np.float32), self.img.affine)
+        fname = ''
+        nib.save(self.mask_img, fname + '_binary_mask.nii.gz')
+        nib.save(self.b0_img, fname + '_mask.nii.gz')
+
+        '''sli = self.data.shape[2] // 2
+        plt.figure('Brain segmentation')
+        plt.subplot(1, 2, 1).set_axis_off()
+        plt.imshow(histeq(self.data[:, :, sli].astype('float')).T,
+                   cmap='gray', origin='lower')
+
+        plt.subplot(1, 2, 2).set_axis_off()
+        plt.imshow(histeq(self.b0_mask[:, :, sli].astype('float')).T,
+                   cmap='gray', origin='lower')
+        plt.show()'''
+
+        
+
+
+
+
+file=Preprocessing(bvecs_file,bvals_file,dti4d_file,mni_template)
+file.brain_segmentation()
