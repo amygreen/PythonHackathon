3080cfed358d6053a7c1685bde9338bc54636e32
omritomer1 <omritomer1@mail.tau.ac.il>
2018-06-28 09:37:26 +0300

    tests for zscores

--- PythonHackathon/zscores.py
+++ PythonHackathon/zscores.py
@@ -12,16 +12,18 @@ class SubjectAnalyzer:
         self.sd_nii_path = sd_nii_path
         self.atlas_nii_path = atlas_nii_path
 
+        # Read nii images:
         self.load_data()
+        # If data is OK, continue to analysis:
         if self.is_data_proper:
-            self.calculate_zscore()
-            self.calculate_atlas_zscores()
-        else:
+            self.calculate_zscore() # Calculate voxel z-scores
+            self.calculate_atlas_results() # Calculate atlas areas mean values and z-scores
+        else: # If data dimensions do not fit, output an error message detailing the error
             self.error_message = \
-                "The following inputs: {}{}{} have an inconsistent have a dimension mismatch with the subject"
-            self.error_message.format('mean map, ' if self.is_mean_proper else '',
-                'st. dev. map, ' if self.is_sd_proper else '',
-                'atlas, ' if self.is_atlas_proper else '')
+                "The following inputs: {}{}{}have an inconsistent have a dimension mismatch with the subject".format(
+                    'mean map, ' if not self.is_mean_proper else '',
+                    'st. dev. map, ' if not self.is_sd_proper else '',
+                    'atlas, ' if not self.is_atlas_proper else '')
 
     def load_data(self):
         self.subject_img = nib.load(self.subject_nii_path)
@@ -52,7 +54,7 @@ class SubjectAnalyzer:
         zscores[np.isnan(zscores)] = 0
         self.significant_zscores = np.where(np.abs(zscores)<=1.96,np.nan,zscores)
 
-    def calculate_atlas_zscores(self):
+    def calculate_atlas_results(self):
         vals = np.zeros(self.atlas_data.max())
         zs = np.zeros(self.atlas_data.max())
         for i in range(1,self.atlas_data.max()+1):
